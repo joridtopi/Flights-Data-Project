1f185d4cd1183dc6ff19e6e21173f85746fe8aab
Jorid Topi <jorid.topi@gmail.com>
2020-07-05 19:32:42 -0400

    adding first JSON data file and modified html page to fetch it

---
+++ Data/Queries/Data Exports/yearly_aircraft_count.sql
@@ -0,0 +1,15 @@
+COPY (SELECT row_to_json(yearly_ac_data) FROM
+(SELECT fl_year, carrier_name, count
+FROM airline_aircraft
+WHERE fl_year > 1994 AND
+	  (carrier_name = 'Alaska Airlines Inc.' OR
+      carrier_name = 'American Airlines Inc.' OR
+      carrier_name = 'Delta Air Lines Inc.' OR
+      carrier_name = 'Frontier Airlines Inc.' OR
+      carrier_name = 'Hawaiian Airlines Inc.' OR
+      carrier_name = 'JetBlue Airways' OR
+      carrier_name = 'Southwest Airlines Co.' OR
+      carrier_name = 'Spirit Air Lines' OR
+      carrier_name = 'United Air Lines Inc.')
+ORDER BY fl_year ASC) yearly_ac_data
+) TO 'path/daily_dep_delay.json'
