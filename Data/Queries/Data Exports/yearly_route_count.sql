cc6b0fad25f828c07856baa9be2d85780aebacda
Jorid Topi <jorid.topi@gmail.com>
2020-07-05 19:21:17 -0400

    adding first JSON data file and modified html page to fetch it

---
+++ Data/Queries/Data Exports/yearly_route_count.sql
@@ -0,0 +1,14 @@
+COPY (SELECT row_to_json(yearly_route_data) FROM
+(SELECT fl_year, count, carrier_name
+FROM airline_routes
+WHERE carrier_name = 'Alaska Airlines Inc.' OR
+      carrier_name = 'American Airlines Inc.' OR
+      carrier_name = 'Delta Air Lines Inc.' OR
+      carrier_name = 'Frontier Airlines Inc.' OR
+      carrier_name = 'Hawaiian Airlines Inc.' OR
+      carrier_name = 'JetBlue Airways' OR
+      carrier_name = 'Southwest Airlines Co.' OR
+      carrier_name = 'Spirit Air Lines' OR
+      carrier_name = 'United Air Lines Inc.'
+ ORDER BY fl_year ASC) yearly_route_data
+) TO 'path/daily_dep_delay.json'
