6081ba5228c81ad9aed44538df1dc32de0b31c4d
Jorid Topi <jorid.topi@gmail.com>
2020-08-03 03:01:45 -0400

    re-organizing data structure/flow

---
+++ Data/Queries/Yearly Analysis/Total Flights/Num_AC_Used_Year/Num_AC_Used_Year.sql
@@ -0,0 +1,280 @@
+
+CREATE TABLE Num_AC_Used_Year AS
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2019
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2018
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2017
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2016
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2015
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2014
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2013
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2012
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2011
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2010
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2009
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2008
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2007
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2006
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2005
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2004
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2003
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2001
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_2000
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1999
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1998
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1997
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1996
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1995
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1994
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1993
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1992
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1991
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1990
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1989
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
+
+INSERT INTO Num_AC_Used_Year
+SELECT fl_year,
+		COUNT(*) AS Num_AC_Used_Year FROM
+	(SELECT EXTRACT(YEAR from fl_date) AS fl_year,
+			tail_num
+	FROM flights_1988
+	GROUP BY fl_year, tail_num) as temp
+GROUP BY fl_year;
